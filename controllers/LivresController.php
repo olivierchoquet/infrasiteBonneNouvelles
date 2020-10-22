c779012145d95cb6b81e98acf6065b49e4489bfa
olivierchoquet <42567743+olivierchoquet@users.noreply.github.com>
2020-10-22 14:04:34 +0200

    Update LivresController.php

--- controllers/LivresController.php
+++ controllers/LivresController.php
@@ -4,7 +4,7 @@ class LivresController{
     private $_db;
 
 	public function __construct() {
-        //$this->_db = Db::getInstance();
+        $this->_db = Db::getInstance();
 	}
 			
 	public function run(){
@@ -38,7 +38,8 @@ class LivresController{
             $html_motcle=htmlspecialchars($_POST['keyword']); # Protection faille XSS pour l'affichage
         } else {
             # Sélection de tous les livres sous forme de tableau
-            $tablivres=Db::getInstance()->select_livres();
+		var_dump("coucou");
+            $tablivres=$this->_db->select_livres();
         }
 		
 		# Ecrire ici la vue livres.php
