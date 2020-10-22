488e13a445b5454c81bf7b96ab2e48ad0e4c64aa
olivierchoquet <42567743+olivierchoquet@users.noreply.github.com>
2020-10-22 14:55:00 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class MyTest extends TestCase
+{
+    
+    public function testInstanceCalculator()
+    {
+        $this->assertEquals("YES","YES");
+    }
+
+   
+ }
+ 
+ ?>
