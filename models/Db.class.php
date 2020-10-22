2551cf020998395e513bbf16eac3ae93ae500d12
olivierchoquet <42567743+olivierchoquet@users.noreply.github.com>
2020-10-22 14:25:47 +0200

    Update Db.class.php

--- models/Db.class.php
+++ models/Db.class.php
@@ -7,12 +7,17 @@ class Db
     private function __construct()
     {
         try {
-            $this->_db = new PDO('mysql:host=localhost;dbname=bdbn;charset=utf8', 'root', '');
-            $this->_db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-			$this->_db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
-        } 
-		catch (PDOException $e) {
-		    die('Erreur de connexion à la base de données : '.$e->getMessage());
+            $db = parse_url(getenv("DATABASE_URL"));
+
+		$this->_db = new PDO("pgsql:" . sprintf(
+    			"host=%s;port=%s;user=%s;password=%s;dbname=%s",
+   			 $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+
         }
 
 		catch (PDOException $e) {
